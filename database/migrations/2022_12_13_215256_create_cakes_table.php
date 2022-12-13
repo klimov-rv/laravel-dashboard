53131d1aea5769815c56342820c2c95707c8c095
Klimov Roman <klimov.rv@ya.ru>
2022-12-14 00:53:47 +0300

    php artisan make:model Cake -m

--- database/migrations/2022_12_13_215256_create_cakes_table.php
+++ database/migrations/2022_12_13_215256_create_cakes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBooksTable extends Migration
+class CreateCakesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('cakes', function (Blueprint $table) {
             $table->id();
-            $table->text('name');
-            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('cakes');
     }
 }
